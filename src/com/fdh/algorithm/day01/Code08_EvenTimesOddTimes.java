bfe5bc6461b65ce726c0c58cb7b57b66b97d35e6
daemon <deimon@126.com>
2021-05-25 22:55:48 +0800

    1、数组中某数出现奇数次，其他偶数次，打印此数 2、如何高效的求一个数的十六进制1的个数

---
+++ src/com/fdh/algorithm/day01/Code08_EvenTimesOddTimes.java
@@ -0,0 +1,58 @@
+package com.fdh.algorithm.day01;
+
+/**
+ * 一个数组中有一种数出现了奇数次，其他数都出现了偶数次，怎么找到并打印这种数
+ */
+public class Code08_EvenTimesOddTimes {
+
+
+    public static void printOddTimesNum1(int[] arr) {
+        int eor = 0;
+        for (int i = 0; i < arr.length; i++) {
+            eor ^= arr[i];
+        }
+        System.out.println(eor);
+    }
+
+    // arr中，有两种数，出现奇数次
+    public static void printOddTimesNum2(int[] arr) {
+
+    }
+
+
+
+
+    /**
+     * 如何计算一个整数的二进制形式有多少个1
+     *
+     * @param data
+     * @return
+     */
+    public static int bit1counts(int data) {
+
+        int count = 0;
+
+        while (data != 0) {
+            //获取二进制形式最右侧的1，其他位置都清0
+            int rightOne = data & ((~data) + 1);
+            count++;
+            //data的最右侧1置0，其他保持不变，若该数据不为0，继续获取下一个最右侧1
+            data ^= rightOne;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+
+//        int[] a = {5, 5, 6, 6, 6};
+//        printOddTimesNum1(a);
+//        int[] a1 = {5, 5, 5, 6, 6, 6};
+//        printOddTimesNum1(a1);
+//        printOddTimesNum2(a1);
+
+        Integer a = 255;
+        int bit1counts = bit1counts(255);
+        System.out.println(bit1counts);
+    }
+}
