75406a68cd6ad5999fa3f7baf3379cc475a501dd
daemon <deimon@126.com>
2021-05-27 19:37:30 +0800

    数组和栈的优缺点比较

--- src/com/fdh/algorithm/day02/Code04_DoubleLink2Stack.java
+++ src/com/fdh/algorithm/day02/Code04_DoubleLink2Stack.java
@@ -4,12 +4,19 @@ import java.util.EmptyStackException;
 
 /**
  * 双向链表实现栈，头插法
+ * 链表的特点：
+ * 在内存中，不要求连续。
+ * 每一个数据都保存了下一个数据的内存地址(指针)，通过这个指针指向下一个数据。
+ * 增加和删除数据很容易。 （增加一个数，不需要动后面的数据，直接改变指针指向就行）
+ * 查找效率低，只能重头开始依次遍历寻找。
+ * 不指定大小，扩展方便。链表大小不用定义，数据随意增删。
  */
-public class DoubleLink2Stack {
+public class Code04_DoubleLink2Stack {
 
     private DoubleNode head;
 
     private DoubleNode tail;
+
     /**
      * 头插法
      *
@@ -54,7 +61,7 @@ public class DoubleLink2Stack {
 
     public static void main(String[] args) {
 
-        DoubleLink2Stack doubleLink2Stack = new DoubleLink2Stack();
+        Code04_DoubleLink2Stack doubleLink2Stack = new Code04_DoubleLink2Stack();
         for (int i = 0; i < 20; i++) {
             doubleLink2Stack.addFromHead(i);
         }
