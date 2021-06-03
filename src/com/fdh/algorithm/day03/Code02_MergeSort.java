cfe2f72cdb8247621660b85437c4eb7e6bd27c64
daemon <deimon@126.com>
2021-06-03 18:16:03 +0800

    归并排序ces

--- src/com/fdh/algorithm/day03/Code02_MergeSort.java
+++ src/com/fdh/algorithm/day03/Code02_MergeSort.java
@@ -1,5 +1,7 @@
 package com.fdh.algorithm.day03;
 
+import com.fdh.algorithm.util.ArrayUtil;
+
 /**
  * 归并排序
  * <p>
@@ -29,19 +31,19 @@ public class Code02_MergeSort {
         if (leftIndex == rightIndex) {
             return;
         }
-        int midIndex = leftIndex + (rightIndex - leftIndex) >> 1;
+        int midIndex = leftIndex + ((rightIndex - leftIndex) >> 1);
         process(arr, leftIndex, midIndex);
         process(arr, midIndex + 1, rightIndex);
-        merge1(arr, leftIndex, midIndex, rightIndex);
+        merge(arr, leftIndex, midIndex, rightIndex);
     }
 
-    private static void merge1(int[] arr, int leftIndex, int midIndex, int rightIndex) {
+    private static void merge(int[] arr, int leftIndex, int midIndex, int rightIndex) {
         int[] temp = new int[rightIndex - leftIndex + 1];
         int i = 0;
         int p1 = leftIndex;
         int p2 = midIndex + 1;
         //mid 左右连边有序,那边小，index就近1逼近直到>=
-        if (p1 <= midIndex && p2 <= rightIndex) {
+        while (p1 <= midIndex && p2 <= rightIndex) {
             temp[i++] = arr[p1] < arr[p2] ? arr[p1++] : arr[p2++];
         }
         if (p1 <= midIndex) {
@@ -56,12 +58,54 @@ public class Code02_MergeSort {
         }
     }
 
-    public static void main(String[] args) {
-        int[] a = {5, 4};
-        mergeSort(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
+    // 非递归方法实现
+    public static void mergeSort2(int[] arr) {
 
+
+        int step = 1;
+        while (step < arr.length) {
+            int leftIndex = 0;
+            int midIndex = 0;
+            int rightIndex = 0;
+            step *= 2;
+            for (int j = 0; j < arr.length / step; j++) {
+                leftIndex = j * step;
+                rightIndex = (j + 1) * step - 1;
+                if ((arr.length - 1 - rightIndex) < step) {//不够一个步长了
+                    rightIndex = arr.length - 1;
+                }
+                //判断
+                midIndex = leftIndex + ((rightIndex - leftIndex) >> 1);
+                merge(arr, leftIndex, midIndex, rightIndex);
+            }
         }
+
+    }
+
+    public static void main(String[] args) {
+
+
+        int[] a = {8, 5, 4, 6,2};
+        int[] copyArray = ArrayUtil.copyArray(a);
+        mergeSort(a);
+        mergeSort2(copyArray);
+        ArrayUtil.printArray(a);
+        ArrayUtil.printArray(copyArray);
+//        int maxSize = 20;
+//        int maxValue = 22;
+//        for (int i = 0; i < 50000; i++) {
+//            int[] randomArray = ArrayUtil.generatorRandomArray(maxSize, maxValue);
+//            int[] copyArray = ArrayUtil.copyArray(randomArray);
+//            mergeSort(randomArray);
+//            mergeSort2(copyArray);
+//            if (!ArrayUtil.isArrayEqual(randomArray, copyArray)) {
+//                System.out.println("完蛋了");
+//                ArrayUtil.printArray(randomArray);
+//                ArrayUtil.printArray(copyArray);
+//                break;
+//            }
+//
+//        }
+
     }
 }
