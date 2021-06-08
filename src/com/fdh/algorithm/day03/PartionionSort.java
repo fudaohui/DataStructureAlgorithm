b6b6ee742bcc54ab13c77ab96d8fa4a16851bd1f
daemon <deimon@126.com>
2021-06-08 16:42:19 +0800

    快速排序，荷兰国旗问题解决

--- src/com/fdh/algorithm/day03/PartionionSort.java
+++ src/com/fdh/algorithm/day03/PartionionSort.java
@@ -32,15 +32,18 @@ public class PartionionSort {
 
     /**
      * 根据给定的数组，和目标值，把小于targer的放左边，大于target的放右边，等于target的放中间
+     * 荷兰国旗问题
+     * 时间复杂度O(n)
      *
      * @param arr
      * @param target
      */
-    public static void partion2(int[] arr, int l, int r, int target) {
+    public static int[] partion2(int[] arr, int l, int r, int target) {
 
         int lessIndex = l - 1;
         int greaterIndex = r + 1;
         int index = l;
+        int count = 0;
         while (index < greaterIndex) {
             if (arr[index] < target) {
                 swap(arr, index++, ++lessIndex);
@@ -50,16 +53,38 @@ public class PartionionSort {
                 index++;
             }
         }
+        return new int[]{lessIndex, greaterIndex};
 
+    }
+
+
+    /**
+     * 快速排序 时间复杂度 O（N*logN）,O(logN)->记录 [l-lessThan,lessThan+1],[moreThan,r]
+     *
+     * @param arr
+     * @param l
+     * @param r
+     */
+    public static void quickSort(int[] arr, int l, int r) {
+        if (l >= r) {
+            return;
+        }
+        int target = arr[l + ((r - l) >> 1)];
+        int[] ints = partion2(arr, l, r, target);
+        quickSort(arr, l, ints[0]);
+        quickSort(arr, ints[1], r);
     }
 
 
     public static void main(String[] args) {
 
-        int[] a = {0, 5, 4, 3, 2};
+//        int[] a = {0, 5, 4, 3, 2};
+//        int[] a = {5, 4, 3, 2, 1, 0};
 //        int[] a = {5, 3, 2, 3, 2};
+        int[] a = {5, 3, 2, 3, 2, 10, 3, 1, 0, 11};
 //        int[] a = {1,2,3,4,5};
-        partion2(a, 0, a.length - 1, 3);
+//        partion2(a, 0, a.length - 1, 3);
+        quickSort(a, 0, a.length - 1);
         ArrayUtil.printArray(a);
     }
 }
