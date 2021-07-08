8148d483d50413fb9de3d841b68e8fc20df1d949
daemon <deimon@126.com>
2021-07-08 09:46:14 +0800

    图形化打印一颗二叉树 1、顺时针打印，“右头左”遍历的方式 2、遍历过程中获得节点的深度，便于空格填充

--- src/com/fdh/algorithm/day07/Code06_PrintBT.java
+++ src/com/fdh/algorithm/day07/Code06_PrintBT.java
@@ -4,8 +4,12 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 /**
- * 打印一颗二叉树，见图：二叉树打印顺时针旋转.jpg
- * 采用右头左方式遍历，然后按照行打印
+ * 打印一颗二叉树，这里打印的方法是顺时针90度旋转打印，
+ * 即先把二叉树选择90度，从上到下依次打印，可以观察到println的顺序
+ * 依次为：{7，3，6，1，5，2，4}，实际上是“右头左”遍历树，和前序遍历打印
+ * 注：
+ * 1、注意遍历的时候也可以拿到节点所在树的深度，便于空格填充
+ * 2、该打印方法暂时不考虑超长数据
  */
 public class Code06_PrintBT {
 
@@ -32,43 +36,54 @@ public class Code06_PrintBT {
     }
 
     public static void printBT(BTNode header) {
-        int btDepth = Code04_TreeMaxWidthAndDepth.countBTDepth(header);
+//        int btDepth = Code04_TreeMaxWidthAndDepth.countBTDepth(header);
 //        int width = Code04_TreeMaxWidthAndDepth.countTreeMaxWidth(header);
 
         Queue<BTNodedepth> btNodedepths = new LinkedList<>();
 
-        rightHeadLeftRtravelBT(header, btNodedepths);
-
-        for (int i = 0; i < btNodedepths.size(); i++) {
+        rightHeadLeftRtravelBT(header, 1, btNodedepths);
+        int size = btNodedepths.size();
+        for (int i = 0; i < size; i++) {
             BTNodedepth btNodedepth = btNodedepths.poll();
-            mprint(btDepth - btNodedepth.getDepth(), btNodedepth.getValue());
+            mprint(btNodedepth.getDepth(), btNodedepth.getValue());
         }
     }
 
     public static void mprint(int depth, int v) {
         StringBuilder stringBuilder = new StringBuilder("");
-        for (int i = 0; i < depth; i++) {
-            stringBuilder.append(" ");
+        for (int i = 1; i < depth; i++) {
+            stringBuilder.append("  ");
         }
         String toString = stringBuilder.append(v).toString();
         System.out.println(toString);
     }
 
     //右，头，左遍历收集节点数据
-    public static int rightHeadLeftRtravelBT(BTNode header, Queue<BTNodedepth> btNodedepthQueue) {
+    public static void rightHeadLeftRtravelBT(BTNode header, int depth, Queue<BTNodedepth> btNodedepthQueue) {
         if (header == null) {
-            return 0;
+            return;
         }
-        int depth = rightHeadLeftRtravelBT(header.getRight(), btNodedepthQueue) + 1;//中序遍历右子树
+        rightHeadLeftRtravelBT(header.getRight(), depth + 1, btNodedepthQueue);//中序遍历右子树
         BTNodedepth btNodedepth = new BTNodedepth();
         btNodedepth.setValue(header.getValue());
         btNodedepth.setDepth(depth);
         btNodedepthQueue.add(btNodedepth);
-        int depth1 = rightHeadLeftRtravelBT(header.getLeft(), btNodedepthQueue);//中序遍历右子树
-        return Math.max(depth, depth1);
+        rightHeadLeftRtravelBT(header.getLeft(), depth + 1, btNodedepthQueue);//中序遍历右子树
 
     }
 
+    /**
+     * 打印结果
+     * 7
+     * 3
+     * 6
+     * 1
+     * 5
+     * 2
+     * 4
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         BTNode head = new BTNode(1);
         head.setLeft(new BTNode(2));
